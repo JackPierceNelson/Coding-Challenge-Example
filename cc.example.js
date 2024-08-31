7c6490f95aac0a34a177a288ce5ea0f5dbdacdb5
Jack Pierce Nelson <Jackpiercenelson@outlook.commailto:Jackpiercenelson@outlook.com>
2024-08-30 19:55:33 -0500

    Task 2 - Order Management System

--- cc.example.js
+++ cc.example.js
@@ -7,3 +7,14 @@ var isPremiumMenber = false;
 console.log(customerName, typeof(customerName));
 console.log(customerID, typeof(customerID));
 console.log(isPremiumMenber, typeof(isPremiumMenber));
+
+// Task 2: Compound Data types
+
+let orderItems = ["Macbook Pro", "Keyboard", "Mouse"];
+const orderDetails = {
+    orderID: 123,
+    items: orderItems,
+    totalAmount: 2459.00,
+}
+
+console.log(orderItems, orderDetails);
